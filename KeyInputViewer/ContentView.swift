695e981a2f814335a7536109dbd1829489c296d5
Masumi Kawasaki <geeknees@gmail.com>
2021-11-30 23:47:11 +0900

    Initial Commit

---
+++ KeyInputViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeyInputViewer
+//
+//  Created by masumi on 2021/11/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KeyInputViewer/KeyInputViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyInputViewerApp.swift
+//  KeyInputViewer
+//
+//  Created by masumi on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct KeyInputViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
